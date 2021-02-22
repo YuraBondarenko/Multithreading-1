f13ac3224deddc80abeb970117725e686d23f00a
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-22 17:31:02 +0200

    added logging

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,11 +1,14 @@
+import org.apache.log4j.Logger;
+
 public class Counter {
+    private static final Logger logger = Logger.getLogger(Counter.class);
     private static final int MIN = 0;
     private static final int MAX = 100;
     private int counter = MIN;
 
     public void counter(String threadName) {
         for (; counter < MAX; counter++) {
-            System.out.println(threadName + " value = " + counter);
+            logger.info(threadName + " value = " + counter);
         }
     }
 }
