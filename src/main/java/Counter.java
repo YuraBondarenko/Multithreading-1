aff5c170c12c40cc3b044de64998a6d470d0b0f4
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-22 13:06:19 +0200

    created constants

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -1,8 +1,10 @@
 public class Counter {
-    private static int i = 0;
+    private static final int MIN = 0;
+    private static final int MAX = 100;
+    private static int i = MIN;
 
     public static void counter(String threadName) {
-        for (; i < 100; i++) {
+        for (; i < MAX; i++) {
             System.out.println(threadName + " value = " + i);
         }
     }
