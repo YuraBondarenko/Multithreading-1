52998313d36721fcfbc90ab01a7274b7353528ea
YuraBondarenko <yuriybondarenko2001@gmail.com>
2021-02-24 14:01:36 +0200

    added tests

--- src/test/java/second/CustomExecutorServiceTest.java
+++ src/test/java/second/CustomExecutorServiceTest.java
@@ -1,21 +1,43 @@
 package second;
 
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class CustomExecutorServiceTest {
-    private static CustomList customList;
-    private static CustomExecutorService customExecutorService;
+public class CustomExecutorServiceTest {
+    private static ListOfNumbersCreator listOfNumbersCreator;
+    private CustomExecutorService customExecutorService;
 
     @BeforeAll
-    static void beforeAll() {
-        customList = new CustomList();
-        customExecutorService = new CustomExecutorService(customList);
+    public static void beforeAll() {
+        listOfNumbersCreator = new ListOfNumbersCreator();
     }
 
     @Test
-    void sum_Ok() {
-        Assertions.assertEquals(customList.sum(), customExecutorService.sum());
+    public void sum_Ok() {
+        List<Integer> list = listOfNumbersCreator.getList(0, 1_000_000);
+        customExecutorService = new CustomExecutorService(list);
+        Assertions.assertEquals(list
+                .stream()
+                .reduce(0, Integer::sum), customExecutorService.getSum());
+    }
+
+    @Test
+    public void negativeNumbers_Ok() {
+        List<Integer> list = listOfNumbersCreator.getList(-100, -1);
+        customExecutorService = new CustomExecutorService(list);
+        Assertions.assertEquals(list
+                .stream()
+                .reduce(0, Integer::sum), customExecutorService.getSum());
+    }
+
+    @Test
+    public void empty_Ok() {
+        List<Integer> list = listOfNumbersCreator.getList(0, 0);
+        customExecutorService = new CustomExecutorService(list);
+        Assertions.assertEquals(list
+                .stream()
+                .reduce(0, Integer::sum), customExecutorService.getSum());
     }
 }
--- src/test/java/second/CustomForkJoinTest.java
+++ src/test/java/second/CustomForkJoinTest.java
@@ -1,21 +1,43 @@
 package second;
 
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class CustomForkJoinTest {
-    private static CustomList customList;
-    private static CustomForkJoin customForkJoin;
+public class CustomForkJoinTest {
+    private static ListOfNumbersCreator listOfNumbersCreator;
+    private CustomForkJoin customForkJoin;
 
     @BeforeAll
     static void beforeAll() {
-        customList = new CustomList();
-        customForkJoin = new CustomForkJoin(customList.getList());
+        listOfNumbersCreator = new ListOfNumbersCreator();
     }
 
     @Test
     void sum_Ok() {
-        Assertions.assertEquals(customList.sum(), customForkJoin.compute());
+        List<Integer> list = listOfNumbersCreator.getList(0, 1_000_000);
+        customForkJoin = new CustomForkJoin(list);
+        Assertions.assertEquals(list
+                .stream()
+                .reduce(0, Integer::sum), customForkJoin.compute());
+    }
+
+    @Test
+    public void negativeNumbers_Ok() {
+        List<Integer> list = listOfNumbersCreator.getList(-100, -1);
+        customForkJoin = new CustomForkJoin(list);
+        Assertions.assertEquals(list
+                .stream()
+                .reduce(0, Integer::sum), customForkJoin.compute());
+    }
+
+    @Test
+    public void empty_Ok() {
+        List<Integer> list = listOfNumbersCreator.getList(0, 0);
+        customForkJoin = new CustomForkJoin(list);
+        Assertions.assertEquals(list
+                .stream()
+                .reduce(0, Integer::sum), customForkJoin.compute());
     }
 }
